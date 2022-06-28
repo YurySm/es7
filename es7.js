2a95ea3e8383bc5f1c3dffced80bd5993739a108
Yury Smolko <49496382+YurySm@users.noreply.github.com>
2022-06-28 22:08:35 +0300

    Add files via upload

---
+++ es7.js
@@ -0,0 +1,9 @@
+// 1. Array.prototype.includes()
+// Функция includes() вводит более читаемый синтаксис для проверки, содержит ли массив элемент, возвращая в зависимости от этого true или false.
+if (![1, 2].includes(3)) {
+    console.log("Not found"); // Not found
+}
+
+// 2. Оператор возведения в степень
+console.log(Math.pow(4, 2) === 4 ** 2); // true
+ 
